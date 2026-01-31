23ac2f3f257a4dc3d9c4c161f9891c0086831d08
manoranjant <manoranjan.th@gmail.com>
2026-01-31 19:37:18 +1100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/weatherproject/",
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
